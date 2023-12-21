709d78acdce88eefeb6112b1393aa48e9fbff032
musicus <musicus@gmail.com>
2023-12-20 23:06:32 -0700

    Update to purpose.

--- Chapter_1_Introduction.jl
+++ Chapter_1_Introduction.jl
@@ -9,5 +9,16 @@ md"""
 # Chapter 1: Introduction
 """
 
+# ╔═╡ 30749254-8d86-491f-9fff-c5bc047aaff3
+md"""
+## Aren't there enough tools? 
+
+No, there will never be enough tools. We can re-use a lot, and we should, but languages, platforms, operating systems, computer systems fall out of favor at an ever rapid pace reflecting the ephemeral experience of one's own existence. Once one system has been mastered, a new one will emerge, and another will simply not be supported anymore, even the one that has not been created yet.
+
+The approach then should be to know and learn the concepts and algorithms behind what exists in music theory itself, and to be able to apply these concepts or re-express these basic algorithms in any programming language. In this work the approach will be based on being able to program music theoretical algorithms in any programming language in order to be free from planned obsolescence.
+
+"""
+
 # ╔═╡ Cell order:
 # ╟─a07d0c82-9fb4-11ee-1b11-d5935ee99aaa
+# ╟─30749254-8d86-491f-9fff-c5bc047aaff3
