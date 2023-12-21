d8b612a0a58d68b961c2df0e2ce8b91ca772549e
musicus <musicus@gmail.com>
2023-12-20 23:11:45 -0700

    Footnote added.

--- Chapter_1_Introduction.jl
+++ Chapter_1_Introduction.jl
@@ -13,10 +13,11 @@ md"""
 md"""
 ## Aren't there enough tools? 
 
-No, there will never be enough tools. We can re-use a lot, and we should, but languages, platforms, operating systems, computer systems fall out of favor at an ever rapid pace reflecting the ephemeral experience of one's own existence. Once one system has been mastered, a new one will emerge, and another will simply not be supported anymore, even the one that has not been created yet.
+No, there will never be enough tools. We can re-use a lot, and we should, but languages, platforms, operating systems, computer systems fall out of favor at an ever rapid pace reflecting the ephemeral experience of one's own existence. Once one system has been mastered, a new one will emerge, and another will simply not be supported anymore, even the one that has not been created yet.[^1]
 
 The approach then should be to know and learn the concepts and algorithms behind what exists in music theory itself, and to be able to apply these concepts or re-express these basic algorithms in any programming language. In this work the approach will be based on being able to program music theoretical algorithms in any programming language in order to be free from planned obsolescence.
 
+[^1]: Some may blame capitalism, others biological evolution, but it is undoubtedly part of our reality in our contemporary world of the early 21st century.
 """
 
 # ╔═╡ Cell order:
