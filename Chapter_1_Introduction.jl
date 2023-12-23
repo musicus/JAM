aeeae33b96c05352a25a64b153967420f57d465b
musicus <musicus@gmail.com>
2023-12-22 17:09:36 -0700

    Edits.

--- Chapter_1_Introduction.jl
+++ Chapter_1_Introduction.jl
@@ -11,7 +11,7 @@ md"""
 
 # ╔═╡ 30749254-8d86-491f-9fff-c5bc047aaff3
 md"""
-## Aren't there enough tools? 
+## Aren't there enough tools already? 
 
 No, there will never be enough tools. We can re-use a lot, and we should, but languages, platforms, operating systems, computer systems fall out of favor at an ever rapid pace reflecting the ephemeral experience of our own existence. Once one system has been mastered, a new one will emerge, and another will simply not be supported anymore, even the one that has not been created yet.[^1]
 
@@ -19,7 +19,7 @@ The approach then should focus on learning the concepts and algorithms behind wh
 
 The primary algorithms will be derived from contemporary post-tonal analyses practices of western music theory.
 
-All examples are categorized and demonstrated primarily using the [Julia programming language](https://julialang.org). [Python](https://www.python.org) also will be used, especially in conjunction with [music21](https://web.mit.edu/music21/), along with the [Humdrum toolkit](https://www.humdrum.org). Some demos will feature [Pd (Pure Data)](http://msp.ucsd.edu/software.html) examples, an open-source visual dataflow programming language used by many intermedia artists, musicians, and composers.[^2]   
+All examples are categorized and demonstrated primarily using the [Julia programming language](https://julialang.org). [Python](https://www.python.org) also will be used, especially in conjunction with [music21](https://web.mit.edu/music21/), along with the [Humdrum toolkit](https://www.humdrum.org). Some demos will feature [Pd (Pure Data)](http://msp.ucsd.edu/software.html) examples, an open-source visual dataflow programming language used by many intermedia artists, musicians, and composers.[^2] Other demos yet will feature the [ChucK](https://chuck.cs.princeton.edu) language, a text-based audio processing/synthesis language.   
 
 [^1]: Some may blame capitalism, others biological evolution, but it is undoubtedly part of our reality in the contemporary world of the early 21st century.
 [^2]: **Pd** has been in existence since the late nineties and because it is openly maintained primarily by Miller S. Puckette, and many other enthusiastic users, it will possibly outlive many platforms maintained by corporate entities.
